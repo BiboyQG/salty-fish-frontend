55d93d75d43a172b9ad61d30232267cc82ef7ac3
Banghao Chi <banghao2@illinois.edu>
2023-12-08 17:38:53 -0600

    t2

--- src/components/Chatbox/Chatbox.js
+++ src/components/Chatbox/Chatbox.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './Chatbox.css'; // Ensure you have the corresponding CSS file
+import React, { useState, useEffect, useRef } from "react";
+import "./Chatbox.css"; // Ensure you have the corresponding CSS file
 
 const Chatbox = () => {
   const [messages, setMessages] = useState([]);
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [isTyping, setIsTyping] = useState(false);
   const [isMinimized, setIsMinimized] = useState(false); // New state to track if chatbox is minimized
   const messagesEndRef = useRef(null);
@@ -17,19 +17,23 @@ const Chatbox = () => {
   }, [messages]);
 
   const sendMessage = async () => {
-    if (input.trim() !== '') {
-      const newMessage = { author: 'user', text: input };
+    if (input.trim() !== "") {
+      const newMessage = { author: "user", text: input };
       setMessages([...messages, newMessage]);
       setIsTyping(true); // Set bot as typing
-      const response = await fetch('http://localhost:5000/chat', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+      const response = await fetch("https://salty-fish-api.onrender.com/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
       });
       const data = await response.json();
       setIsTyping(false); // Reset bot typing status
-      setMessages([...messages, newMessage, { author: 'bot', text: data.reply }]);
-      setInput('');
+      setMessages([
+        ...messages,
+        newMessage,
+        { author: "bot", text: data.reply },
+      ]);
+      setInput("");
     }
   };
 
@@ -38,9 +42,9 @@ const Chatbox = () => {
   };
 
   return (
-    <div className={`chatbox ${isMinimized ? 'minimized' : ''}`}>
-      <button onClick={toggleMinimize}> 
-        {isMinimized ? 'Maximize' : 'Minimize'}
+    <div className={`chatbox ${isMinimized ? "minimized" : ""}`}>
+      <button onClick={toggleMinimize}>
+        {isMinimized ? "Maximize" : "Minimize"}
       </button>
       <div className="messages">
         {messages.map((msg, index) => (
@@ -48,7 +52,9 @@ const Chatbox = () => {
             {msg.text}
           </div>
         ))}
-        {isTyping && <div className="typing-status">The Salty Fish Bot is typing...</div>}
+        {isTyping && (
+          <div className="typing-status">The Salty Fish Bot is typing...</div>
+        )}
         <div ref={messagesEndRef} /> {/* Empty div for scrolling reference */}
       </div>
       <div className="input-area">
@@ -56,7 +62,7 @@ const Chatbox = () => {
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          onKeyPress={(e) => e.key === 'Enter' && sendMessage()}
+          onKeyPress={(e) => e.key === "Enter" && sendMessage()}
         />
         <button onClick={sendMessage}>Send</button>
       </div>
